0520b369c4824972bd7d7e92999d252b3cccd154
leandro-adal <lleandro.adal@gmail.com>
2023-07-08 19:14:07 -0300

    ADD: Console.log

---
+++ 01 - Console.log/exercicio/index.js
@@ -0,0 +1,3 @@
+// Criar a frase: Meu nome é "LEANDRO". Estou aprendendo JavaScript às 7 da noite. Com o 7 fora da String.
+
+console.log('Meu nome é "LEANDRO". Estou aprendendo JavaScript às' , 7 , 'da noite.');
---
+++ 01 - Console.log/index.js
@@ -0,0 +1,11 @@
+console.log('Hello world!');
+console.log("Hello world!");
+console.log(`Hello world!`);
+
+console.log('Hello "world"!');
+console.log("Hello 'world'!");
+console.log(`"Hello" 'world'!`);
+
+console.log(123, 15254.263, 'Hello world')
+
+// console.log - As coisas colocas nele so aparecem no console, não aparecendo na página. Ele bom pra debugar o código.
