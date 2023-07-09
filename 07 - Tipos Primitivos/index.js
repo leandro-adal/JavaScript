6842a5a532f0aba0f1ec7016c7ad4b75c37c0fec
leandro-adal <lleandro.adal@gmail.com>
2023-07-09 03:59:21 -0300

    ADD: Tipos primitivos

---
+++ 07 - Tipos Primitivos/index.js
@@ -0,0 +1,8 @@
+// String, number, boolean, undefined, null, symbol
+
+const nome = 'Leandro'; // String
+const num1 = 12; // Number
+const num2 = 14.58; // Number
+let nomeAluno; // Undefined (automático). Não aponta para algum lugar na memoria.
+const sob = null; // Nulo. Não aponta para algum lugar na memoria.
+const aprovado = true; // Boolean
