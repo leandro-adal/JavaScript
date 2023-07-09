d2c63d2023c8504459cc642660a7e05b2d14f5fb
leandro-adal <lleandro.adal@gmail.com>
2023-07-09 00:16:30 -0300

    ADD: variável (let)

---
+++ 04 - Variáveis (let)/index.js
@@ -0,0 +1,11 @@
+// Não pode começar com números as variáveis.
+//Não pode conter espaços ou traços.
+// Case-sensitive. Abc diferente de abc
+// Não pode declarar posteriormente a mesma variável com let.
+let nome = 'João';
+
+console.log(nome, 'nasceu no Brasil');
+console.log(nome, 'nasceu nos USA');
+
+nome = 'Otávio';
+console.log(nome);
