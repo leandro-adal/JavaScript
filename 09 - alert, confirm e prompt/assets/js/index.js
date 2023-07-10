58f9c1085b4059e54b9eab010a22278b37fb4569
leandro-adal <lleandro.adal@gmail.com>
2023-07-10 07:28:39 -0300

    ADD: alert, confirm e prompt

---
+++ 09 - alert, confirm e prompt/assets/js/index.js
@@ -0,0 +1,7 @@
+alert('error');
+
+// Confirmar retorna True e False
+let confirma = confirm('Confirm');
+
+// O prompt retorna uma string digitada pelo usuário para
+let num1 = prompt('Digite uma mensagem');
---
+++ 09.1 - Exercicio/09 - alert, confirm e prompt/assets/js/index.js
@@ -0,0 +1,6 @@
+const num1 = prompt('Digite um número!');
+const num2 = prompt('Digite outro número!');
+
+const resultado = parseFloat(num1) + parseFloat(num2);
+
+alert('O resultado foi: ' + resultado);
