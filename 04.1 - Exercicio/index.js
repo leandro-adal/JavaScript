7153daa7030babbbc008ce97ee4cf827df7fa8e9
leandro-adal <lleandro.adal@gmail.com>
2023-07-10 08:03:21 -0300

    UP: exercício 04

--- 04.1 - Exercicio/index.js
+++ 04.1 - Exercicio/index.js
@@ -2,9 +2,13 @@ let varA = 'A';
 let varB = 'B';
 let varC = 'C';
 
+/*
 const varD = varA
 varA = varB
 varB = varC
 varC = varD
+*/
+
+[varA, varB, varC] = [varB, varC, varA];
 
 console.log(varA, varB, varC)
