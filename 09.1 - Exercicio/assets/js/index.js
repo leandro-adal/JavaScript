be5c73285a0a12aee8438c8cbcc2c6cb0bf2e7bc
leandro-adal <lleandro.adal@gmail.com>
2023-07-10 17:54:41 -0300

    UP: exercício 09

--- 09.1 - Exercicio/assets/js/index.js
+++ 09.1 - Exercicio/assets/js/index.js
@@ -1,6 +1,6 @@
 const num1 = prompt('Digite um número!');
 const num2 = prompt('Digite outro número!');
 
-const resultado = parseFloat(num1) + parseFloat(num2);
+const resultado = Number(num1) + Number(num2);
 
 alert(`O resultado foi: ${resultado}`);
