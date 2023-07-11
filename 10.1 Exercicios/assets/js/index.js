8bae10adbc574a1fb046ec285db7168b428f9929
leandro-adal <lleandro.adal@gmail.com>
2023-07-10 23:32:17 -0300

    ADD: Exercícios

---
+++ 10.1 Exercicios/assets/js/index.js
@@ -0,0 +1,13 @@
+
+const nome = prompt('Digite seu nome completo:');
+const nomeSemEspaco = nome.replaceAll(' ', '');
+
+document.body.innerHTML += `Seu nome é: ${nome}<br />`;
+document.body.innerHTML += `Seu nome tem ${nomeSemEspaco.length} letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]}<br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra LETRA no seu nome? ${nome.indexOf('Leandro')} <br />`;
+document.body.innerHTML += `Qual o último índice da letra LETRA no seu nome? ${nome.lastIndexOf('Leandro')} <br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-4)} <br />`;
+document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')}<br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()}<br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()}<br />`;
