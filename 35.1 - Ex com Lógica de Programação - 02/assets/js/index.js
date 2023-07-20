a601f7c532702bea900f264084c6fb17e4185514
leandro-adal <lleandro.adal@gmail.com>
2023-07-20 20:16:41 -0300

    UP: Ex. Logica 2

--- 35.1 - Ex com Lógica de Programação - 02/assets/js/index.js
+++ 35.1 - Ex com Lógica de Programação - 02/assets/js/index.js
@@ -1,12 +1,14 @@
 // Função que recebe largura e altura e return true se a imagem estiver no modo paisagem
 function ePaisagem(largura, altura) {
-  if (largura < altura) return false;
-  else return true;
+  return largura >= altura; // Não precisa do return pois a expressão ja retorna verdadeiro ou falso
 }
+
+// Declarando as variáveis e chamando a função
 const num1 = 1080;
 const num2 = 1920;
 console.log(ePaisagem(num1, num2));
 
-// Caso a situação de serem iguais fosse ignorável
-const max = (largura, altura) => largura < altura ? false : true;
-console.log(max(num1, num2));
+// Simplificando
+const ePaisagem2 = (largura, altura) => largura >= altura; 
+
+console.log(ePaisagem2(num1, num2));
